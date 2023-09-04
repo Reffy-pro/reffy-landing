e1e7ddf81243f2a8be34ba83682521990fe444fa
Aliaksandr Antropau <aliaksandr.antropau@inspirebrands.com>
2023-09-04 22:34:49 +0400

    Update lib versions

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,9 @@ import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import compress from "astro-compress";
-import image from "@astrojs/image";
 
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind(), compress(), image()],
-  experimental: {
-    contentCollections: true,
-  },
+  integrations: [react(), tailwind(), compress()],
 });
